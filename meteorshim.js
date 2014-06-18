a92a9c79cb036a7d0429f6eb9671448a96b74bbc
Dan Gilkerson <contact@dangilkerson.com>
2014-06-17 17:38:57 -0700

    Read production.json file for settings

--- meteorshim.js
+++ meteorshim.js
@@ -6,6 +6,8 @@ process.env.ROOT_URL = "http://" + process.env.OPENSHIFT_APP_DNS || "localhost";
 process.env.MONGO_URL = (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+process.env.METEOR_SETTINGS = fs.readFileSync('production.json', 'utf8');
+//process.env.DDP_DEFAULT_CONNECTION_URL="ddp+sockjs://<appname>-taskinout.rhcloud.com:8443/sockjs";
 
 // Show connection details on startup
 console.log("MONGO_URL IS: " + process.env.MONGO_URL);
