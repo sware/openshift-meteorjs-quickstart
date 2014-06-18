fce4906a66c193a5569a45c82c0ce53141ef3d07
Dan Gilkerson <contact@dangilkerson.com>
2014-06-18 10:11:30 -0700

    Get mongourl from meteor settings file

--- meteorshim.js
+++ meteorshim.js
@@ -1,12 +1,14 @@
 #!/bin/env node
-var fs = require('fs')
+var fs = require('fs');
 
 // Setup env
+var settings = fs.readFileSync('production.json', 'utf8');
+var meteorSettings = JSON.parse(settings);
 process.env.ROOT_URL = "http://" + process.env.OPENSHIFT_APP_DNS || "localhost";
-process.env.MONGO_URL = (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
+process.env.MONGO_URL = meteorSettings.mongoUrl || (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-process.env.METEOR_SETTINGS = fs.readFileSync('production.json', 'utf8');
+process.env.METEOR_SETTINGS = settings;
 process.env.DDP_DEFAULT_CONNECTION_URL="ddp+sockjs://" + process.env.OPENSHIFT_GEAR_DNS + ":8443/sockjs";
 
 // Show connection details on startup
